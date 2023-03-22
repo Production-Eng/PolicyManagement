746a757602467f1ea9cce8ad651d4734ab74b607
Dennis-Soare <79460847+Dennis-Soare@users.noreply.github.com>
2023-03-22 22:16:06 +0200

    new clients endpoint

---
+++ src/main/java/ro/unibuc/hello/models/Client.java
@@ -0,0 +1,39 @@
+package ro.unibuc.hello.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    int idClient;
+    private  String name;
+    private String cnp;
+
+    public int getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(int idClient) {
+        this.idClient = idClient;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCnp() {
+        return cnp;
+    }
+
+    public void setCnp(String cnp) {
+        this.cnp = cnp;
+    }
+}
